d6d30db3a78c196a6f1bcac1a9ff3602b4725cb7
Gizmodude01 <138951311+Gizmodude01@users.noreply.github.com>
2024-03-17 22:07:42 -0400

    Update verifyLegalAge.py

--- Single Purpose Projects/verifyLegalAge.py
+++ Single Purpose Projects/verifyLegalAge.py
@@ -1,7 +1,7 @@
 try:
     if (age := int(input("Please enter your age: "))) >= 18 and age < 120:
         print(f"User is {age} and is allowed to vote")
-    elif age > 120:
+    elif age > 120 or age <= 3:
         print(f"{age} is not valid, please enter a real age")
     else: 
          print(f"User is {age} and is NOT allowed to vote")
